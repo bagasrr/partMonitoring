bf699fb8d1d3b432d7ad207be7f74cf8c21f07ab
bagasrr <bagasramadhan239@gmail.com>
2025-02-13 13:42:57 +0700

    add Color to Swapped Part

--- frontend/src/utils/format.js
+++ frontend/src/utils/format.js
@@ -5,16 +5,13 @@ export const formatDate = (date) => {
 };
 
 export const FormatStatusColor = (status, red, yellow, green, blue) => {
-  switch (true) {
-    case status.includes(red):
-      return "bg-red-100 text-red-700";
-    case status.includes(yellow):
-      return "bg-yellow-100 text-yellow-700";
-    case status.includes(green):
-      return "bg-green-100 text-green-700";
-    case status.includes(blue):
-      return "bg-blue-100 text-blue-700";
-    default:
-      return "bg-white text-gray-700"; // default
-  }
+  return status.includes(red)
+    ? "bg-red-100 text-red-700"
+    : [yellow].flat().some((y) => status.includes(y))
+    ? "bg-yellow-100 text-yellow-700"
+    : status.includes(green)
+    ? "bg-green-100 text-green-700"
+    : status.includes(blue)
+    ? "bg-blue-100 text-blue-700"
+    : "bg-white text-gray-700"; // default
 };
